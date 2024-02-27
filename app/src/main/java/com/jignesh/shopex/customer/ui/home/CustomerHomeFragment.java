cfdb4d778b67f13069990d46038ece1fc6c8b2ad
jigne <jigneshrathod154@gmail.com>
2024-02-27 17:38:09 +0530

    CustomerActivity, ShopkeeperActivity with both side Navigations and more 34 files related navigations 27 Feb 2024 5:37

---
+++ app/src/main/java/com/jignesh/shopex/customer/ui/home/CustomerHomeFragment.java
@@ -0,0 +1,37 @@
+package com.jignesh.shopex.customer.ui.home;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+
+import com.jignesh.shopex.databinding.FragmentCustomerHomeBinding;
+
+public class CustomerHomeFragment extends Fragment {
+
+    private FragmentCustomerHomeBinding binding;
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        HomeViewModel homeViewModel =
+                new ViewModelProvider(this).get(HomeViewModel.class);
+
+        binding = FragmentCustomerHomeBinding.inflate(inflater, container, false);
+        View root = binding.getRoot();
+
+        final TextView textView = binding.textHome;
+        homeViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+        return root;
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+}
